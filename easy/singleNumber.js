76ef64ff2bf7d92dbb05a05094e183c8d0a728a4
Anuj <anuj01062004@gmail.com>
2025-08-04 17:56:21 +0530

    single number

---
+++ easy/singleNumber.js
@@ -0,0 +1,73 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,2,1]
+
+// Output: 1
+
+// Example 2:
+
+// Input: nums = [4,1,2,1,2]
+
+// Output: 4
+
+// Example 3:
+
+// Input: nums = [1]
+
+// Output: 1
+
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 3 * 104
+// -3 * 104 <= nums[i] <= 3 * 104
+// Each element in the array appears twice except for one element which appears only once.
+
+
+//have an array of intergers -> contain both negative and positive values, all elements occur twice except for one.
+//return that single occured element
+
+// Example 1:
+
+// Input: nums = [2,2,1]
+
+// Output: 1
+
+// Example 2:
+
+// Input: nums = [4,1,2,1,2]
+
+// Output: 4
+
+// Example 3:
+
+// Input: nums = [1]
+
+// Output: 1
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let result = 0
+    for(let n of nums){
+       result = result ^ n
+    }
+    return result
+};
+
+
+//test cases 
+
+console.log(singleNumber([2,2,1]), 'should be 1')
+console.log(singleNumber([4,1,2,1,2]), 'should be 4')
+console.log(singleNumber([1]), 'should be 1')
