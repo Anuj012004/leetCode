ff71b04ad24b8b2ce41f836dd83163f62a0cb66d
Anuj <anuj01062004@gmail.com>
2025-08-22 23:33:09 +0530

    optimal

--- easy/buyAndSellStock.js
+++ easy/buyAndSellStock.js
@@ -33,16 +33,34 @@
 /**
  * @param {number[]} prices
  * @return {number}
- */
+//  */
 var maxProfit = function(prices) {
-    let maxProfit = 0
-    for(let i=0; i<prices.length; i++){
-        for(let j=i+1; j<prices.length; j++){
-            let profit = prices[j]-prices[i]
-            if(profit>maxProfit){
-                maxProfit = profit
-            }
-        }
-    }
-    return maxProfit
+//     let maxProfit = 0
+//     for(let i=0; i<prices.length; i++){
+//         for(let j=i+1; j<prices.length; j++){
+//             let profit = prices[j]-prices[i]
+//             if(profit>maxProfit){
+//                 maxProfit = profit
+//             }
+//         }
+//     }
+//     return maxProfit
+//optimal approach 
+
+    let minVal = Infinity;
+    let maxProfit = 0;
+
+    for(let x of prices){
+        if(x < minVal){
+            minVal = x;
+        };
+        let profit = x - minVal;
+
+        if(profit > maxProfit){
+            maxProfit = profit;
+        };
+    };
+    return maxProfit;
 };
+
+
