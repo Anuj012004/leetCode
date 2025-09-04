79f31c0a43c2c87d6bedc059c08cf41fb28679f6
Anuj <anuj01062004@gmail.com>
2025-09-04 23:27:22 +0530

    find closest person

---
+++ easy/findClosestPerson.js
@@ -0,0 +1,102 @@
+
+// Code
+// Testcase
+// Testcase
+// Test Result
+// 3516. Find Closest Person
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given three integers x, y, and z, representing the positions of three people on a number line:
+
+// x is the position of Person 1.
+// y is the position of Person 2.
+// z is the position of Person 3, who does not move.
+// Both Person 1 and Person 2 move toward Person 3 at the same speed.
+
+// Determine which person reaches Person 3 first:
+
+// Return 1 if Person 1 arrives first.
+// Return 2 if Person 2 arrives first.
+// Return 0 if both arrive at the same time.
+// Return the result accordingly.
+
+ 
+
+// Example 1:
+
+// Input: x = 2, y = 7, z = 4
+
+// Output: 1
+
+// Explanation:
+
+// Person 1 is at position 2 and can reach Person 3 (at position 4) in 2 steps.
+// Person 2 is at position 7 and can reach Person 3 in 3 steps.
+// Since Person 1 reaches Person 3 first, the output is 1.
+
+// Example 2:
+
+// Input: x = 2, y = 5, z = 6
+
+// Output: 2
+
+// Explanation:
+
+// Person 1 is at position 2 and can reach Person 3 (at position 6) in 4 steps.
+// Person 2 is at position 5 and can reach Person 3 in 1 step.
+// Since Person 2 reaches Person 3 first, the output is 2.
+
+// Example 3:
+
+// Input: x = 1, y = 5, z = 3
+
+// Output: 0
+
+// Explanation:
+
+// Person 1 is at position 1 and can reach Person 3 (at position 3) in 2 steps.
+// Person 2 is at position 5 and can reach Person 3 in 2 steps.
+// Since both Person 1 and Person 2 reach Person 3 at the same time, the output is 0.
+
+ 
+
+// Constraints:
+
+// 1 <= x, y, z <= 100
+
+
+//three persons x,y,z  => these are between 0 and 101 ie 1 to 100
+
+//x is the posi of person 1 and y is the posi of person 2 and z is the posi of person 3 who remains in rest,
+//x and y moves towards z at same speed find who reaches first.
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function(x, y, z) {
+    let stepsOfX = 0
+   let stepsOfY = 0
+   if(x>z){
+    stepsOfX += x-z
+   }else{
+    stepsOfX += z-x
+   }
+   if(y>z){
+    stepsOfY +=y-z
+   }else{
+    stepsOfY += z-y
+   }
+   if(stepsOfX<stepsOfY){
+    return 1
+   }else if(stepsOfX>stepsOfY){
+    return 2
+   }else{
+    return 0
+   }
+};
