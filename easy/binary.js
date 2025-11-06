d56eba41dd9abea9b89b20f5165fc003f9cb8bc9
Anuj <anuj01062004@gmail.com>
2025-11-06 23:39:34 +0530

    brute and optimal solution for find the index of the target value from the nums

---
+++ easy/binary.js
@@ -0,0 +1,67 @@
+// 704. Binary Search
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+// Explanation: 9 exists in nums and its index is 4
+// Example 2:
+
+// Input: nums = [-1,0,3,5,9,12], target = 2
+// Output: -1
+// Explanation: 2 does not exist in nums so return -1
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -104 < nums[i], target < 104
+// All the integers in nums are unique.
+// nums is sorted in ascending order.
+
+
+//we have an array and a target, nums=>cannot be empty,contains integers, -ve and +ve, no string, special vals,is in ascending order,no duplicate number
+//target -> an integer value, 
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    //brute force loop
+//       for(let i=0; i<nums.length; i++){
+//     if(nums[i]===target){
+//         return i
+//     }
+//    }
+//     return -1
+
+//binary optimal
+
+    let left = 0
+    let right = nums.length-1
+
+    while(left <= right){
+        let mid = Math.floor((left+right)/2)
+        if(nums[mid]===target){
+            return mid
+        }
+        else if(nums[mid]<target){
+            left = mid +1
+        }else if(nums[mid]>target){
+            right = mid -1
+        }
+    }
+    return -1
+};
