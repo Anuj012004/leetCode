b2d65feb5be35f80b789230576d9fd084251506d
Anuj <anuj01062004@gmail.com>
2025-11-28 01:38:27 +0530

    valid anagram using MAP

--- easy/validAnagram.js
+++ easy/validAnagram.js
@@ -43,8 +43,28 @@ var isAnagram = function(s, t) {
 
     // return S === T? true:false
 
+    // if(s.length !== t.length) return false;
+    // return s.split('').sort().join('') === t.split('').sort().join('');
+
+       // if(s.length !== t.length) return false;
+    // return s.split('').sort().join('') === t.split('').sort().join('');
     if(s.length !== t.length) return false;
-    return s.split('').sort().join('') === t.split('').sort().join('');
+    let map1 = new Map()
+    let map2 = new Map()
+    for(let char of s){
+        map1.set(char, (map1.get(char)||0)+1)
+    }
+     for(let char of t){
+        map2.set(char, (map2.get(char)||0)+1)
+    }
+
+    for(let [key,value] of map1){
+        if(map2.get(key)!==value){
+            return false
+        }
+    }
+
+    return true
 
 
 };
