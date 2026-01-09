f3c2aaeb9d4acedd70ff8f0ade8c4c3ea5220caf
Anuj <anuj01062004@gmail.com>
2026-01-09 23:32:17 +0530

    use memo

--- easy/fibonacci.js
+++ easy/fibonacci.js
@@ -53,3 +53,16 @@ var fib = function(n) {
 
     return fib(n-1) + fib(n-2)
 };
+
+
+// using dp
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n, memo={}) {
+    if(n in memo) return memo[n]
+    if(n<=1) return n
+    memo[n] = fib(n-1,memo)+fib(n-2,memo)
+    return memo[n]
+};
