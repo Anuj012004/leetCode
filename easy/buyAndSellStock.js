9022ece5b183ba9eaae864433d089d11c4d867b5
Anuj <anuj01062004@gmail.com>
2026-01-17 20:31:56 +0530

    time to buy and sell

--- easy/buyAndSellStock.js
+++ easy/buyAndSellStock.js
@@ -66,18 +66,35 @@ var maxProfit = function(prices) {
 
     //------------------------------------------
 
-    let minVal = prices[0]
-    let maxProfit = 0
-    for(let i=1;i<prices.length;i++){
-        if(prices[i]<minVal){
-            minVal = prices[i]
-        }
-        let profit = prices[i]-minVal
-        if(profit> maxProfit){
-            maxProfit = profit
-        }
+    // let minVal = prices[0]
+    // let maxProfit = 0
+    // for(let i=1;i<prices.length;i++){
+    //     if(prices[i]<minVal){
+    //         minVal = prices[i]
+    //     }
+    //     let profit = prices[i]-minVal
+    //     if(profit> maxProfit){
+    //         maxProfit = profit
+    //     }
+    // }
+    // return maxProfit
+
+
+    /**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+   let minPrice = prices[0]
+   let maxProfit = 0
+   for(let i=1; i<prices.length; i++){
+    if(prices[i]<minPrice){
+        minPrice = prices[i]
     }
-    return maxProfit
+    maxProfit=Math.max(maxProfit,prices[i]-minPrice)
+   }
+   return maxProfit
+};
 };
 
 
