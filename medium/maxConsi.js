a849d7fbcc163af68d1c1df57938f4a0e44f572d
Anuj <anuj01062004@gmail.com>
2026-02-08 23:48:11 +0530

    max consi

---
+++ medium/maxConsi.js
@@ -0,0 +1,46 @@
+// 485. Max Consecutive Ones
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+// Example 2:
+
+// Input: nums = [1,0,1,1,0,1]
+// Output: 2
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// nums[i] is either 0 or 1.
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+        let current = 0
+    let maxCount = 0
+
+    for (let num of nums) {
+        if (num === 1) {
+            current++
+            maxCount = Math.max(maxCount, current);
+        } else {
+            current = 0
+        }
+    }
+
+    return maxCount
+};
