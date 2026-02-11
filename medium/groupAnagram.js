19706da47178958aacdb978354134f9ad48aa39d
Anuj <anuj01062004@gmail.com>
2026-02-11 15:24:00 +0530

    gropu anagram use frequency count of char instead of sorting them optimal code than sorting

--- medium/groupAnagram.js
+++ medium/groupAnagram.js
@@ -97,6 +97,28 @@ var groupAnagrams = function(strs) {
     return [...map.values()]
 };
 
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs){
+    let map = new Map()
+    for(let str of strs){
+        let arr = new Array(26).fill(0)
+
+        for(let char of str){
+            arr[char.charCodeAt(0)-97]++
+        }
+        let key = arr.join('#')
+
+        if(!map.has(key)){
+            map.set(key,[])
+        }
+        map.get(key).push(str)
+    }
+    return [...map.values()]
+};
+
 
 //Test Cases
 console.log(groupAnagrams( ["eat","tea","tan","ate","nat","bat"]),'it output should be ["bat"],["nat","tan"],["ate","eat","tea"]')
