6c6094e9826acafd0f14c9360f368cccab3c7eb5
Anuj <anuj01062004@gmail.com>
2026-03-03 13:27:46 +0530

    valid parenthesis

--- easy/validParentheses.js
+++ easy/validParentheses.js
@@ -75,3 +75,34 @@ var isValid = function(s) {
 
     return stack.length === 0;
 };
+
+       // string -> consist of specific char, cannot be empty 
+        //bool -> true or false
+        //true -> open bracekets are closed as well
+        // closed in correct order
+        //every clsoed brackets has open  bracket
+
+        // if all these conditions are statisfied -> true else false
+
+        //create a stack
+        //loop on char of s 
+        // if char are (,{,[ push to the stack
+        // else grab the top element from the stack if char of s !== top with all those
+        //three conditions return false
+        //return the stack.length===0
+
+
+        let stack = []
+        for(let char of s){
+            if(char==='(' || char==='{' || char === '[' ){
+                stack.push(char)
+            }else{
+                let top = stack.pop()
+                if(
+                    (char===')' && '('!==top) || (char==='}' && '{'!==top) || (char===']' && '['!==top)) {
+                    return false
+                }
+            }
+
+        }
+        return stack.length===0
