dfe59257843601fc27a71c9f27dfa4e1ec7b079b
Anuj <anuj01062004@gmail.com>
2026-03-17 22:43:35 +0530

    max sum

---
+++ easy/maxPairSum.js
@@ -0,0 +1,90 @@
+// 2815. Max Pair Sum in an Array
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an integer array nums. You have to find the maximum sum of a pair of numbers from nums such that the largest digit in both numbers is equal.
+
+// For example, 2373 is made up of three distinct digits: 2, 3, and 7, where 7 is the largest among them.
+
+// Return the maximum sum or -1 if no such pair exists.
+
+ 
+
+// Example 1:
+
+// Input: nums = [112,131,411]
+
+// Output: -1
+
+// Explanation:
+
+// Each numbers largest digit in order is [2,3,4].
+
+// Example 2:
+
+// Input: nums = [2536,1613,3366,162]
+
+// Output: 5902
+
+// Explanation:
+
+// All the numbers have 6 as their largest digit, so the answer is 2536 + 3366 = 5902.
+
+// Example 3:
+
+// Input: nums = [51,71,17,24,42]
+
+// Output: 88
+
+// Explanation:
+
+// Each number's largest digit in order is [5,7,7,4,4].
+
+// So we have only two possible pairs, 71 + 17 = 88 and 24 + 42 = 66.
+
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 100
+// 1 <= nums[i] <= 104
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSum = function(nums) {
+     let map = new Map()
+    let ans = -1
+
+    for(let num of nums){
+        let digit = getMaxDigit(num)
+
+        if(!map.has(digit)){
+            map.set(digit, [])
+        }
+
+        let arr = map.get(digit)
+        arr.push(num)
+        arr.sort((a,b)=>b-a)
+
+        if(arr.length > 2) arr.pop() // keep only top 2
+
+        if(arr.length === 2){
+            ans = Math.max(ans, arr[0] + arr[1])
+        }
+    }
+
+    return ans
+};
+
+function getMaxDigit(num){
+    let max = 0
+    while(num > 0){
+        max = Math.max(max, num % 10)
+        num = Math.floor(num / 10)
+    }
+    return max
+};
