255603d640d49ed9d23f592bc8f0fd44c54ca5ea
Anuj <anuj01062004@gmail.com>
2026-03-27 22:31:56 +0530

    add digit

---
+++ easy/addDigit.js
@@ -0,0 +1,43 @@
+// 258. Add Digits
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+ 
+
+// Example 1:
+
+// Input: num = 38
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2 
+// Since 2 has only one digit, return it.
+// Example 2:
+
+// Input: num = 0
+// Output: 0
+ 
+
+// Constraints:
+
+// 0 <= num <= 231 - 1
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+  while (num >= 10) {
+        let sum = 0
+        while (num > 0) {
+            sum += num % 10
+            num = Math.floor(num / 10)
+        }
+        num = sum
+    }
+    return num
+};
