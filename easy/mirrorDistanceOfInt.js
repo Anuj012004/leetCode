c3cf8f31fe3361506b9de5acf66a970b18a1a80d
Anuj <anuj01062004@gmail.com>
2026-04-18 12:18:48 +0530

    mirror Distance Of Int

---
+++ easy/mirrorDistanceOfInt.js
@@ -0,0 +1,73 @@
+// 3783. Mirror Distance of an Integer
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an integer n.
+
+// Define its mirror distance as: abs(n - reverse(n))​​​​​​​ where reverse(n) is the integer formed by reversing the digits of n.
+
+// Return an integer denoting the mirror distance of n​​​​​​​.
+
+// abs(x) denotes the absolute value of x.
+
+ 
+
+// Example 1:
+
+// Input: n = 25
+
+// Output: 27
+
+// Explanation:
+
+// reverse(25) = 52.
+// Thus, the answer is abs(25 - 52) = 27.
+// Example 2:
+
+// Input: n = 10
+
+// Output: 9
+
+// Explanation:
+
+// reverse(10) = 01 which is 1.
+// Thus, the answer is abs(10 - 1) = 9.
+// Example 3:
+
+// Input: n = 7
+
+// Output: 0
+
+// Explanation:
+
+// reverse(7) = 7.
+// Thus, the answer is abs(7 - 7) = 0.
+ 
+
+// Constraints:
+
+// 1 <= n <= 109
+
+
+// integer -> n -> non-negative and non zero, always an integer no str,sym,or anything special
+
+// int -> denoting mirror distance of integer
+//mirror dis = n - mirrorImage || rev(n)
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var mirrorDistance = function(n) {
+    //find the rev of n i.e,, mirror image
+    //to rev conver to string, split, rev, join
+    //return mirror distance
+    let rev = n.toString().split('').reverse().join('')
+    return Math.abs(n-rev)
+    
+};
+
+console.log(mirrorDistance(25)) // 27
+console.log(mirrorDistance(10)) // 9
