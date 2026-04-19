a1a2f17f93bc31e7db15235d32a7c0ca94324780
Anuj <anuj01062004@gmail.com>
2026-04-19 22:37:27 +0530

    optimal using pointers

--- medium/maxDisBetPairVal.js
+++ medium/maxDisBetPairVal.js
@@ -64,3 +64,25 @@ var maxDistance = function(nums1, nums2) {
     }
     return maxDis
 };
+
+//optimal two pointers 
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxDistance = function(nums1, nums2) {
+    let i = 0
+    let j = 0
+    let maxDis = 0
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] <= nums2[j]) {
+            maxDis = Math.max(maxDis, j - i)
+            j++
+        } else {
+            i++
+        }
+    }
+    return maxDis
+};
