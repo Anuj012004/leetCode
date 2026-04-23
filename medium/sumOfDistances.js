a174e2d0bae15c6f405febcaf535c4e067fe8fda
Anuj <anuj01062004@gmail.com>
2026-04-23 11:51:34 +0530

    sum of dis

--- medium/sumOfDistances.js
+++ medium/sumOfDistances.js
@@ -52,4 +52,48 @@ var distance = function(nums) {
     return arr
 };
 
+//
+//optimal
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var distance = function(nums) {
+    let map = new Map();
+    let res = new Array(nums.length).fill(0);
+
+    //  group indices
+    for (let i = 0; i < nums.length; i++) {
+        if (!map.has(nums[i])) map.set(nums[i], []);
+        map.get(nums[i]).push(i);
+    }
+
+    // process each group
+    for (let indices of map.values()) {
+        let n = indices.length;
+
+        let prefixSum = 0;
+
+        // Left to right
+        for (let i = 0; i < n; i++) {
+            res[indices[i]] += indices[i] * i - prefixSum;
+            prefixSum += indices[i];
+        }
+
+        // Reset for right side
+        prefixSum = 0;
+
+        // Right to left
+        for (let i = n - 1; i >= 0; i--) {
+            res[indices[i]] += prefixSum - indices[i] * (n - 1 - i);
+            prefixSum += indices[i];
+        }
+    }
+
+    return res;
+
+};
+
 console.log(distance([1,2,4,1,1,4] ))
+
