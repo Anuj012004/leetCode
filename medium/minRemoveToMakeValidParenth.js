df806d9fe111a318b2fbc0de483200a9f9991889
Anuj <anuj01062004@gmail.com>
2026-06-12 21:36:26 +0530

    min remove to make the valid parenth

---
+++ medium/minRemoveToMakeValidParenth.js
@@ -0,0 +1,66 @@
+// 1249. Minimum Remove to Make Valid Parentheses
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given a string s of '(' , ')' and lowercase English characters.
+
+// Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+// Formally, a parentheses string is valid if and only if:
+
+// It is the empty string, contains only lowercase characters, or
+// It can be written as AB (A concatenated with B), where A and B are valid strings, or
+// It can be written as (A), where A is a valid string.
+
+
+// Example 1:
+
+// Input: s = "lee(t(c)o)de)"
+// Output: "lee(t(c)o)de"
+// Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+// Example 2:
+
+// Input: s = "a)b(c)d"
+// Output: "ab(c)d"
+// Example 3:
+
+// Input: s = "))(("
+// Output: ""
+// Explanation: An empty string is also valid.
+
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s[i] is either '(' , ')', or lowercase English letter.
+
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var minRemoveToMakeValid = function (s) {
+    let arr = s.split('')
+    let stack = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '(') {
+            stack.push(i)
+        } else if (arr[i] === ')') {
+            if (stack.length > 0) {
+                stack.pop()
+            } else {
+                arr[i] = ''
+            }
+        }
+    }
+    while (stack.length) {
+        arr[stack.pop()] = ''
+    }
+    return arr.join('')
+};
+
+
+console.log(minRemoveToMakeValid("lee(t(c)o)de)"))
