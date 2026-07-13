41e722892bb95057b95995ab7c94dae70c128fe9
Anuj <anuj01062004@gmail.com>
2026-07-14 00:34:05 +0530

    middle of the linked list

---
+++ easy/middleOfLL.js
@@ -0,0 +1,56 @@
+// 876. Middle of the Linked List
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5]
+// Output: [3,4,5]
+// Explanation: The middle node of the list is node 3.
+// Example 2:
+
+
+// Input: head = [1,2,3,4,5,6]
+// Output: [4,5,6]
+// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+// Constraints:
+
+// The number of nodes in the list is in the range [1, 100].
+// 1 <= Node.val <= 100
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    //one pointer move twice of the other 
+    //if twice moved element reaches at the end it means that 
+    // other one is at half of the ll or at the mid 
+
+    let fast = head 
+    let slow = head
+    while(fast != null && fast.next != null){
+        slow = slow.next
+        fast = fast.next.next
+    }
+    return slow
+};
