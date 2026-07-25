57746c53b51b351135313e609c6fe66a75fa8a4b
Anuj <anuj01062004@gmail.com>
2026-07-25 13:29:42 +0530

    convert the arr to binary search tree

---
+++ easy/convertArrToBinaryTree.js
@@ -0,0 +1,51 @@
+// 108. Convert Sorted Array to Binary Search Tree
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+ 
+
+// Example 1:
+
+
+// Input: nums = [-10,-3,0,5,9]
+// Output: [0,-3,9,-10,null,5]
+// Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+// Example 2:
+
+
+// Input: nums = [1,3]
+// Output: [3,1]
+// Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+    let start = 0
+    let end = nums.length-1
+  
+    function populate(arr,start,end){
+        if(start>end) return null
+        let mid = Math.floor((start+end)/2)
+        let root = new TreeNode(arr[mid])
+        root.left = populate(arr,start,mid-1)
+        root.right = populate(arr,mid+1,end)
+        return root
+    }
+    return populate(nums,0,nums.length-1)
+};
