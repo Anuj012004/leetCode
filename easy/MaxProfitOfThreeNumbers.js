1758951153322dc7c89d5129b98525b9002bde53
Anuj <anuj01062004@gmail.com>
2026-07-26 21:47:34 +0530

    o of n complexity

--- easy/MaxProfitOfThreeNumbers.js
+++ easy/MaxProfitOfThreeNumbers.js
@@ -39,3 +39,38 @@ var maximumProduct = function(nums) {
     let product2 = nums[0]*nums[1]*nums[n-1]
     return Math.max(product1,product2)
 };
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+    let num1 = -Infinity
+    let num2 = -Infinity
+    let num3 = -Infinity
+
+    let minNum1 = Infinity
+    let minNum2 = Infinity
+
+    for(let num of nums){
+        if(num>=num1){
+            num3 = num2
+            num2 = num1
+            num1 = num
+        }else if(num<num1 && num>=num2){
+            num3 = num2
+            num2 = num
+        }else if(num>num3 && num<num2){
+            num3 = num
+        }
+
+        if(num<=minNum1){
+            minNum2 = minNum1
+            minNum1 = num
+        }else if(num<minNum2){
+            minNum2 = num
+        }
+    }   
+    return Math.max((num1*num2*num3 ), (minNum1*minNum2*num1))
+};
