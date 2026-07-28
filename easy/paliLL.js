dc77f45e529aef77a528d576990b036aee1bfabc
Anuj <anuj01062004@gmail.com>
2026-07-28 22:25:03 +0530

    optimal

--- easy/paliLL.js
+++ easy/paliLL.js
@@ -54,3 +54,53 @@ var isPalindrome = function(head) {
     return true
     
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    //find the mid
+    //rev the mid
+    //if not equal false else true
+    if(head==null || head.next==null){
+        return true
+    }
+    let fast = head
+    let slow = head
+    while(fast!==null && fast.next!==null){
+        slow = slow.next
+        fast = fast.next.next
+    }
+
+    let prev = null;
+    let curr = slow;
+
+    while (curr !== null) {
+        let next = curr.next;
+
+        curr.next = prev;
+
+        prev = curr;
+        curr = next;
+    }
+
+    let f = head
+    let l = prev
+    while(l!==null){
+        if(f.val !== l.val){
+            return false
+        }
+        f = f.next
+        l = l.next
+    }
+    return true
+
+};
