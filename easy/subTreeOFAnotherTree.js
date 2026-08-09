22364793a985eff4886fb1fe731b6a92821611c9
Anuj <anuj01062004@gmail.com>
2026-08-09 21:08:53 +0530

    sub tree of another tree

---
+++ easy/subTreeOFAnotherTree.js
@@ -0,0 +1,82 @@
+// 572. Subtree of Another Tree
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+
+// A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+// Output: true
+// Example 2:
+
+
+// Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+// Output: false
+ 
+
+// Constraints:
+
+// The number of nodes in the root tree is in the range [1, 2000].
+// The number of nodes in the subRoot tree is in the range [1, 1000].
+// -104 <= root.val <= 104
+// -104 <= subRoot.val <= 104
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+        function isSameTree(root1, root2) {
+        if (root1 === null && root2 === null) {
+            return true;
+        }
+
+        if (root1 === null || root2 === null) {
+            return false;
+        }
+
+        if (root1.val !== root2.val) {
+            return false;
+        }
+
+        return (
+            isSameTree(root1.left, root2.left) &&
+            isSameTree(root1.right, root2.right)
+        );
+    }
+
+    if (subRoot === null) {
+        return true;
+    }
+
+    if (root === null) {
+        return false;
+    }
+
+    if (isSameTree(root, subRoot)) {
+        return true;
+    }
+
+    return (
+        isSubtree(root.left, subRoot) ||
+        isSubtree(root.right, subRoot)
+    );
+};
