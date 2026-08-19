312459b70e729f92e67f3324d84fd48acef01c25
Anuj <anuj01062004@gmail.com>
2026-08-19 21:23:46 +0530

    symmetric trees

---
+++ easy/symmetricTrees.js
@@ -0,0 +1,54 @@
+// 101. Symmetric Tree
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,2,3,4,4,3]
+// Output: true
+// Example 2:
+
+
+// Input: root = [1,2,2,null,3,null,3]
+// Output: false
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 1000].
+// -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    let queue = [[root.left,root.right]]
+    while(queue.length>0){
+        let [left,right] = queue.shift()
+        if(left===null && right===null){
+            continue
+        }
+        if(left===null || right===null || left.val!==right.val){
+            return false
+        }
+        queue.push([left.left, right.right])
+        queue.push([left.right,right.left])
+    }
+    return true
+};
