a02272f209cdeacff8731f76d62f1b63a1d223f6
Anuj <anuj01062004@gmail.com>
2026-08-19 21:29:43 +0530

    rec sol DFS

--- easy/symmetricTrees.js
+++ easy/symmetricTrees.js
@@ -52,3 +52,28 @@ var isSymmetric = function(root) {
     }
     return true
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    function findSym(left,right){
+        if(left===null && right ===null){
+            return true
+        }
+        if(left===null || right===null || left.val!==right.val){
+            return false
+        }
+        return findSym(left.left,right.right) && findSym(left.right,right.left)
+    }
+    return findSym(root.left,root.right)
+};
